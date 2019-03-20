8df5980c3ed5641f774720432e837ef914c095b8
apady <frankchenls@outlook.com>
2019-03-20 21:30:59 +0800

    Update apady_env.sh

--- apady_env.sh
+++ apady_env.sh
@@ -2,7 +2,7 @@
 
 WORK_DIR=/usr/bin
 
-if [[ -z `rpm -qa |grep epel-release` ]]
+if [[ -z `rpm -qa |grep epel-release` ]];then
   rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 fi
