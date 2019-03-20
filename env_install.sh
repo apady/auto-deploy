fde705034b7d9ff490446584db86bd78a110980d
Frank Chen <frankchenls@outlook.com>
2019-03-21 01:21:30 +0800

    Update env_install.sh

--- env_install.sh
+++ env_install.sh
@@ -3,9 +3,6 @@
 # Database
 set -x
 
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-
 if [[ -z `which jq` ]]; then
   yum -y install jq
 fi
