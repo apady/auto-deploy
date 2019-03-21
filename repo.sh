16cb7aa7ff8bc15bcae1832f92aa41603070a3ab
apady <frankchenls@outlook.com>
2019-03-22 01:11:25 +0800

    bug fix

--- repo.sh
+++ repo.sh
@@ -4,3 +4,7 @@ if [[ -z `rpm -qa |grep epel-release` ]];then
 	rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 	rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 fi 
+
+if [[ -z `which jq` ]]; then
+  yum -y install jq
+fi 
