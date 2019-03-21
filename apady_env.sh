d64b1848a87bcd0678be05e43ea70c0bf34e1f4f
apady <frankchenls@outlook.com>
2019-03-21 21:25:01 +0800

    optimize

--- apady_env.sh
+++ apady_env.sh
@@ -2,10 +2,6 @@
 
 WORK_DIR=/usr/bin
 
-if [[ -z `rpm -qa |grep epel-release` ]];then
-  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-fi
 
 case "$1"x in
   "deploy"x )
