63f0c662589f3850cd2ca6921d4040ad18b33e3e
apady <frankchenls@outlook.com>
2019-04-04 14:33:07 +0800

    Create apady_env.sh

---
+++ apady_env.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+WORK_DIR=/usr/bin
+if [[ ! -f ${WORK_DIR}/apady_envd/config.json ]]; then
+  echo 'Configuration file does not exit!'
+  exit 1
+
+case "$1"x in
+  "deploy"x )
+    cd ${WORK_DIR}/apady_envd
+    bash ./env_install.sh deploy
+    bash ./deploy.sh
+    ;;
+  "repair"x )
+    cd ${WORK_DIR}/apady_envd
+    bash ./clean.sh 
+    bash ./env_install.sh
+    bash ./deploy.sh
+    ;;
+  "clean"x )
+    cd ${WORK_DIR}/apady_envd
+    bash ./clean.sh all
+    ;;
+  "config"x )
+    cd ${WORK_DIR}/apady_envd
+    python setup.py
+    ;;
+  *)
+    echo "
+      Welcome to use Apady auto deploy tool @author lishen chen 
+
+      @email frankchenls@outlook.com
+
+      Usage: apady_env COMMAND
+
+      List of Commands:
+      deploy   Deploy apady development environment.
+      repair   Clean project source code and reinstall the environment.
+      config   Configure  prerequisite parameters used in the program.  
+      clean    Clean all the development environment."
+  ;;
+
+esac
+# end of file
